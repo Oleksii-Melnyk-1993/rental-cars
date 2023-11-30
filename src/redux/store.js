6396ea14fda5f6eb5655150a406913493d30a121
Melnyk Oleksii <112346850+Oleksii-Melnyk-1993@users.noreply.github.com>
2023-11-30 21:34:19 +0200

    make store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { contactsReducer } from './sliceContacts';
-import { filterReducer } from './filterContacts';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { carsApi } from './operations';
+import favoritesReducer from './slice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+const reducers = combineReducers({
+  [carsApi.reducerPath]: carsApi.reducer,
+  favorites: favoritesReducer,
+});
+
+const persistConfig = {
+  key: 'favorites',
+  version: 1,
+  storage,
+  whiteList: ['favorites'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filter: filterReducer,
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(carsApi.middleware),
 });
+
+export let persistor = persistStore(store);
